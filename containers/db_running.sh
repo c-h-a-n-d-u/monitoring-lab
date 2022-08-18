ec1cc5b16e34b33cc5c91cc30f56ebf7685e1234
c-h-a-n-d-u <chandralekha.chitta@gmail.com>
2022-08-18 11:02:38 +0000

    db running 1

--- containers/db_running.sh
+++ containers/db_running.sh
@@ -1,4 +1,2 @@
-#!bin/sh
-cd /home/grads/hackathon3/monitoring-lab/containers/;
-source ../env-setup; 
-/usr/local/bin/docker-compose ps | /usr/bin/grep -v Exit | /usr/bin/awk '{print $1","$1}'
+#!/usr/bin/sh
+/usr/bin/docker ps -a | /usr/bin/grep -v Exit | /usr/bin/awk '{print $1","$1}'
